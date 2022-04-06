ce1759355b516a791c24d36ed257395fe5ba5692
Lazaroo3 <99274470+Lazaroo3@users.noreply.github.com>
2022-04-06 11:46:28 +0200

    Update ROS_1.py

--- ROS_1.py
+++ ROS_1.py
@@ -2,19 +2,28 @@
 # license removed for brevity
 import rospy
 from std_msgs.msg import String
-def callback(msg):
-    rospy.loginfo("mensaje recibido: %s",msg.data)
 
-def memory_subscriber():
-    rospy.init_node('suscriptor_basico_roberto',anonymous=True)
-    #el primer argumento, es el nombrel del topic al que queremos suscribirnos...
-    #el segundo argumento es el tipo de mensaje,
-    rospy.Subscriber('chatter',String,callback)
-    rospy.spin()
+def talker():
+	rospy.init_node('talker')
+	pub=rospy.Publisher('Ivan',String,queue_size=10)
 
-if __name__ == "__main__":
-    try:
-        memory_subscriber()
-    except rospy.ROSInterruptException:
-        pass
+	rate=rospy.Rate(4) #4hz
+	
+	try:
+		numero = int(input("Dame un numero: "))
+        
+	except ValueError:
+		print("Error, se ha producido un error inesperado")
+		input("Pulse ENTER para finalizar.")
+		exit()
+	
+	mensaje = "Mensaje enviado %i" % numero
 
+	pub.publish(mensaje)
+	rate.sleep()
+
+if __name__=='__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
